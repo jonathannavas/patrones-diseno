2a98e05357c518fd20ebb4e0741d5025ef2002e6
Jonathan <jona.navas@outlook.com>
2025-08-16 16:54:07 -0500

    [imp] builder pattern

--- 01-creacionales/01.2-builder.ts
+++ 01-creacionales/01.2-builder.ts
@@ -50,24 +50,32 @@ class QueryBuilder {
   }
 
   select(...fields: string[]): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.fields = fields;
+    return this;
   }
 
   where(condition: string): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.conditions = [...this.conditions, condition];
+    return this;
   }
 
   orderBy(field: string, direction: 'ASC' | 'DESC' = 'ASC'): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.orderFields.push(`${field} ${direction}`)
+    return this;
   }
 
   limit(count: number): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.limitCount = count;
+    return this;
   }
 
   execute(): string {
-    // Select id, name, email from users where age > 18 and country = 'Cri' order by name ASC limit 10;
-    throw new Error('Method not implemented.');
+      const fields = this.fields.length > 0 ? this.fields.join(', ') : '*';
+      const conditions = this.conditions.length > 0 ? ` WHERE ${this.conditions.join(' and ')}` : ''
+      const orderFields = this.orderFields.length> 0 ? `ORDER BY ${this.orderFields.join(', ')}` : '';
+      const limitCount = this.limitCount ? 'limit ' + this.limitCount : '';
+      // Select id, name, email from users where age > 18 and country = 'Cri' order by name ASC limit 10;
+      return `Select ${fields} from ${this.table} ${conditions} ${orderFields} ${limitCount};`;
   }
 }
 
